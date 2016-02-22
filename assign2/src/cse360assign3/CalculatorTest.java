c3bc27ef272927351e1b57373534c9cd3a09faff
BruceWayneJr <naveenkumar@host-55-106.aztevds.tempe.az.us.clients.pavlovmedia.com>
2016-02-22 12:08:12 -0700

    initial version

---
+++ assign2/src/cse360assign3/CalculatorTest.java
@@ -0,0 +1,62 @@
+package cse360assign3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import cse360assign3.Calculator;
+
+public class CalculatorTest {
+
+	@Test
+	public void testCalculator() 
+	{
+		//Test if the size() returns the total size and not the length
+		Calculator obj = new Calculator();
+		assertNotNull(obj);
+		
+	}
+	@Test
+	public void testgetTotal() 
+	{
+		
+		//Calculator obj = new Calculator();
+		
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testadd() 
+	{
+		//Calculator obj = new Calculator();
+
+		fail("Not yet implemented");
+	}
+	
+
+	
+
+	
+	@Test
+	public void testsubtract() 
+	{
+
+		fail("Not yet implemented");
+	}
+	@Test	
+	public void testmultiply() 
+	{
+		//Calculator obj = new Calculator();
+	
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testdivide() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testgetHistory() {
+		fail("Not yet implemented");
+	}
+
+}
